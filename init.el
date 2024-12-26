1d0f13544a9e7c6712dd3c26a1c35a396442bcdd
makisat <makiya28sato@gmail.com>
2024-12-26 13:22:56 -0500

    pre evil

---
+++ init.el
@@ -0,0 +1,128 @@
+;; -------------- ;;
+;; Initial setups ;;
+;; -------------- ;;
+
+;; No start up message
+(setq inhibit-startup-message t)
+
+;; Remove some visuals
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+
+;; Transparent background
+(set-frame-parameter nil 'alpha-background 80)
+
+;; ESC for quit
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+;; Line numbers
+(global-display-line-numbers-mode t)
+(setq display-line-numbers-type 'relative)
+
+(dolist (mode '(org-mode-hook
+                 term-mode-hook
+                 eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+(setq display-line-numbers-width-start t) ;; Stop the screen shifting
+(setq scroll-margin 8)
+
+;; Change the font
+(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 140)
+
+;; ----------------- ;;
+;; Setup use-package ;;
+;; ----------------- ;;
+
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+                         ("org" . "https://orgmode.org/elpa")
+                         ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; ---------------- ;;
+;; Install packages ;;
+;; ---------------- ;;
+
+;; Mini Buffer ;;
+
+(use-package ivy
+  :bind (:map ivy-minibuffer-map
+          ("TAB" . ivy-alt-done))
+  :config
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file))
+  :config
+  (setq ivy-initial-inputs-alist nil))
+
+(use-package smex) ; enable history for counsel-M-x
+
+(use-package which-key
+  :init (which-key-mode)
+  :config
+  (setq which-key-idle-delay 3))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; Visuals ;;
+
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+
+  (load-theme 'doom-tomorrow-night t)
+
+  (doom-themes-org-config))
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+;; -------------------- ;;
+;; Custom Set Variables ;;
+;; -------------------- ;;
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8" default))
+ '(delete-selection-mode nil)
+ '(package-selected-packages '(ivy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
