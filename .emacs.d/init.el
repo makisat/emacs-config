fd05364a655923265941f9bfea228d1898a1285e
makisat <makiya28sato@gmail.com>
2025-12-17 12:08:59 -0500

    org-roam-ui

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -299,6 +299,13 @@
   :config
   (org-roam-db-autosync-mode))
 
+(use-package org-roam-ui
+    :config
+    (setq org-roam-ui-sync-theme t
+          org-roam-ui-follow t
+          org-roam-ui-update-on-save t
+          org-roam-ui-open-on-start t))
+
 ;; Nerd icons (modern alternative to all-the-icons)
 (use-package nerd-icons
   :custom
